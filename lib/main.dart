296f5d4c984b33e281d0f005c983be063452ad52
FlutLab.io <>
2020-11-15 15:13:38 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -137,7 +137,7 @@ return Container(
                 FlatButton(
                   child: const Text('Edit', style: TextStyle(color: Colors.white)),
                   onPressed: () {
-                    editDataMHS(BuildContext context);
+                    editDataMHS(context);
                   },
                 ),
                 FlatButton(
