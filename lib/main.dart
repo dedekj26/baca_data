ca5d8c59f8935322083313ad3dcbf025403dcfb1
FlutLab.io <>
2020-11-16 15:06:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ Future<void> deleteEmployee(String nim) async {
     final url = 'https://startmyflutter.000webhostapp.com/deleteDatajson.php';
     await http.get(url + '?nim=$nim');
 
-    return MyApp();
+    return this.setState()
 }
 
 // A function that converts a response body into a List<Mhs>.
